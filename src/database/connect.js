5132557db941f15bdb5df444dd5a5b8004168985
Moaz El Gandy <moazhazem2005@icloud.com>
2024-07-03 21:41:47 +0300

    refactor: Update database connection details

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,7 +2,15 @@ import mysql2 from "mysql2";
 
 export const db = mysql2.createConnection({
   host: "b2np4mpxyz4p4pll113r-mysql.services.clever-cloud.com",
-  user: "upe4h82dw5fvadjz",
-  password: "8kZL4ttg84ycDqS0pkdE",
-  database: "b2np4mpxyz4p4pll113r",
+  user: "your_username",
+  password: "your_password",
+  database: "your_database",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to database!");
 });
